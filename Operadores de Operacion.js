3f1082c3fc78c2c1ae53eef7e6672f1bfa9af6b2
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-02-10 12:45:14 -0500

    Create Operadores de Operacion.js

---
+++ Operadores de Operacion.js
@@ -0,0 +1,12 @@
+//Operadores Binarios porque son dos variables
+3 + 2
+50 - 10
+10 * 20
+20 / 2
+ 
+ //Operadores con String, concatenar el otro valor
+
+ 
+ "Juliana " + "Castillo" 
+ 
+ //Concatenacion: tomar dos valores y juntarlos en uno
